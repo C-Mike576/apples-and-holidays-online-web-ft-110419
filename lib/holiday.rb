bba6ca0df65ca479035b24c19b3de0505631219f
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 17:58:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     p season.to_s.capitalize + ":"
     holiday.each do |literal_holiday, value|
-      literal_holiday.to_s.split("_")
+      literal_holiday.to_s.split("_").capitalize
       literal_holiday.capitalize
       literal_holiday.join
       binding.pry
