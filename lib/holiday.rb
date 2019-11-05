7a80ce35175127eb081acf75b80d8ef16cdef099
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 18:20:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    p season.to_s.capitalize + ":"
+    puts season.to_s.capitalize + ":"
     holiday.each do |literal_holiday, value|
       opperate = literal_holiday.to_s.split("_")
       opperate.each do |n|
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
           opperate.join
         end
       end
-      p " #{opperate.join(" ")}: #{value.join(", ")}"
+      puts " #{opperate.join(" ")}: #{value.join(", ")}"
     end
   end
 end
